4d6f97dad8191975a1189c0ff228cac33e9b63ae
noviirna <novi.irnawati@gmail.com>
2021-06-05 15:10:58 +0700

    change command into jidi-bigini

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,29 @@
+#!/usr/bin/env node
+
 const yargs = require("yargs");
 
-function replaceVowelWithinText(text, replacementChar) {
-  if( null === text || 0 === text.length) {
+const replaceVowelWithinText = (text, replacementChar) => {
+  if (null === text || 0 === text.length) {
     return text;
   } else {
-    if(null === replacementChar) {
-      replacementChar = '';
+    if (null === replacementChar) {
+      replacementChar = "";
     }
     return text
       .replace(/[aeiou]/g, replacementChar.toLowerCase())
       .replace(/[AEIOU]/g, replacementChar.toUpperCase());
   }
-}
+};
 
-const input = yargs
- .usage("Masukan teks asli: -n <text>")
- .option("n", { alias: "text", describe: "Your text", type: "string", demandOption: true })
- .argv;
+const input = yargs.usage("Masukan teks asli: -n <text>").option("n", {
+  alias: "text",
+  describe: "Your text",
+  type: "string",
+  demandOption: true,
+}).argv;
 
-
-console.log(replaceVowelWithinText(input.text,'i'));
+console.log("");
+console.log("");
+console.log(replaceVowelWithinText(input.text, "i"));
+console.log("");
+console.log("");
